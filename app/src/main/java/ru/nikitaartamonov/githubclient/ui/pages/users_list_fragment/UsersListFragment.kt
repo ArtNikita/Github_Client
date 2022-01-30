92c2c9a5b4df3fb989500905561ffa8e30b7e4db
Nikita Artamonov <nikitaartamonov@mail.ru>
2022-01-30 16:19:09 +0300

    added blank UsersListFragment.kt

---
+++ app/src/main/java/ru/nikitaartamonov/githubclient/ui/pages/users_list_fragment/UsersListFragment.kt
@@ -0,0 +1,7 @@
+package ru.nikitaartamonov.githubclient.ui.pages.users_list_fragment
+
+import androidx.fragment.app.Fragment
+import ru.nikitaartamonov.githubclient.R
+
+class UsersListFragment : Fragment(R.layout.fragment_users_list) {
+}
