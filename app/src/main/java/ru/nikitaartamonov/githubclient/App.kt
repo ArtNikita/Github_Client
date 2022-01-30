4f718f27634286428015c90a127d2f7df02e1506
Nikita Artamonov <nikitaartamonov@mail.ru>
2022-01-30 16:56:44 +0300

    added extensions to App

--- app/src/main/java/ru/nikitaartamonov/githubclient/App.kt
+++ app/src/main/java/ru/nikitaartamonov/githubclient/App.kt
@@ -1,7 +1,22 @@
 package ru.nikitaartamonov.githubclient
 
 import android.app.Application
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
 class App : Application() {
 
+}
+
+val Context.app
+    get() = applicationContext as App
+
+val Fragment.app
+    get() = requireContext() as App
+
+fun <T> ViewModel.post(liveData: LiveData<T>, data: T) {
+    (liveData as MutableLiveData).postValue(data)
 }
