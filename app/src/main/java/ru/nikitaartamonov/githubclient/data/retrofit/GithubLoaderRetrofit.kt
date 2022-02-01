7c820d5382318473535c0247e0e61d70641ed42f
Nikita Artamonov <nikitaartamonov@mail.ru>
2022-02-01 16:29:13 +0300

    implemented loadRepos method with its calling

--- app/src/main/java/ru/nikitaartamonov/githubclient/data/retrofit/GithubLoaderRetrofit.kt
+++ app/src/main/java/ru/nikitaartamonov/githubclient/data/retrofit/GithubLoaderRetrofit.kt
@@ -6,6 +6,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.nikitaartamonov.githubclient.domain.GithubLoader
+import ru.nikitaartamonov.githubclient.domain.entity.RepoEntity
 import ru.nikitaartamonov.githubclient.domain.entity.UserEntity
 
 class GithubLoaderRetrofit : GithubLoader {
@@ -32,6 +33,27 @@ class GithubLoaderRetrofit : GithubLoader {
             override fun onFailure(call: Call<UserEntity>, error: Throwable) {
                 callback(GithubLoader.Result.Error(error))
             }
+        })
+    }
+
+    override fun loadRepos(userName: String, callback: (GithubLoader.Result) -> Unit) {
+        githubApi.getRepos(userName).enqueue(object : Callback<List<RepoEntity>> {
+            override fun onResponse(
+                call: Call<List<RepoEntity>>, response: Response<List<RepoEntity>>
+            ) {
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        callback(GithubLoader.Result.Success(it))
+                    } ?: callback(GithubLoader.Result.Error(Throwable("Empty response")))
+                } else {
+                    callback(GithubLoader.Result.Error(Throwable("Server problem")))
+                }
+            }
+
+            override fun onFailure(call: Call<List<RepoEntity>>, error: Throwable) {
+                callback(GithubLoader.Result.Error(error))
+            }
+
         })
     }
 }
--- app/src/main/java/ru/nikitaartamonov/githubclient/domain/GithubLoader.kt
+++ app/src/main/java/ru/nikitaartamonov/githubclient/domain/GithubLoader.kt
@@ -1,13 +1,12 @@
 package ru.nikitaartamonov.githubclient.domain
 
-import ru.nikitaartamonov.githubclient.domain.entity.UserEntity
-
 interface GithubLoader {
 
     fun loadUser(userName: String, callback: (Result) -> Unit)
+    fun loadRepos(userName: String, callback: (Result) -> Unit)
 
     sealed class Result {
         data class Error(val error: Throwable) : Result()
-        data class Success(val userEntity: UserEntity) : Result()
+        data class Success<T>(val body: T) : Result()
     }
 }
