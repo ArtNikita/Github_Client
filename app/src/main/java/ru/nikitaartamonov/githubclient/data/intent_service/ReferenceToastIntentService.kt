5c67eb036e8aa56324c93283857c4bccf6e5d121
Nikita Artamonov <nikitaartamonov@mail.ru>
2022-03-09 23:30:12 +0300

    implemented ReferenceToastIntentService.kt

---
+++ app/src/main/java/ru/nikitaartamonov/githubclient/data/intent_service/ReferenceToastIntentService.kt
@@ -0,0 +1,45 @@
+package ru.nikitaartamonov.githubclient.data.intent_service
+
+import android.app.IntentService
+import android.content.Context
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+
+private const val MESSAGE_KEY = "MESSAGE_KEY"
+private const val DELAY_KEY = "DELAY_KEY"
+
+private const val TAG = "@@@"
+
+class ReferenceToastIntentService : IntentService("ReferenceToastIntentService") {
+
+    private var counter = 0
+
+    override fun onHandleIntent(intent: Intent?) {
+        intent?.let {
+            val msg = it.getStringExtra(MESSAGE_KEY) ?: ""
+            val delay = it.getLongExtra(DELAY_KEY, 0)
+            showDelayedToast(msg, delay)
+        }
+    }
+
+    private fun showDelayedToast(msg: String, delay: Long) {
+        val mainHandler = Handler(Looper.getMainLooper())
+        Thread.sleep(delay)
+        val countedMsg = "${counter++}. $msg"
+        mainHandler.post {
+            Toast.makeText(this, countedMsg, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+
+        fun showToast(context: Context, msg: String, delay: Long) {
+            val intent = Intent(context, ReferenceToastIntentService::class.java)
+            intent.putExtra(MESSAGE_KEY, msg)
+            intent.putExtra(DELAY_KEY, delay)
+            context.startService(intent)
+        }
+    }
+}
--- app/src/main/java/ru/nikitaartamonov/githubclient/ui/main/MainActivity.kt
+++ app/src/main/java/ru/nikitaartamonov/githubclient/ui/main/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import ru.nikitaartamonov.githubclient.R
+import ru.nikitaartamonov.githubclient.data.intent_service.ReferenceToastIntentService
 import ru.nikitaartamonov.githubclient.databinding.ActivityMainBinding
 import ru.nikitaartamonov.githubclient.ui.pages.users_list.UsersListFragment
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initToastButton()
         if (savedInstanceState == null) {
             initStartScreen()
         }
@@ -24,6 +27,16 @@ class MainActivity : AppCompatActivity() {
     private fun initStartScreen() {
         supportFragmentManager.commit {
             add(R.id.main_fragment_container, UsersListFragment())
+        }
+    }
+
+    private fun initToastButton() {
+        binding.toastButton.setOnClickListener {
+            ReferenceToastIntentService.showToast(
+                this,
+                UUID.randomUUID().toString(),
+                3_000L
+            )
         }
     }
 }
